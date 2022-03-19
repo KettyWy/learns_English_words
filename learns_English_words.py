72bc392cbed7c75077570c43909a3a4b30b22025
Alexey Isaev <isaev.alex75311@gmail.com>
2022-03-20 01:41:17 +0300

    refactor

--- learns_English_words.py
+++ learns_English_words.py
@@ -2,54 +2,43 @@ import time
 import random
 
 
-def add_value():
-    s = print('Добавьте в словарь слово в формате: "слово:перевод". Для выхода - "3".')
-    while True:
-        s = input()
-        if s == '3':
-            break
-        if s.find(':') >= 0:
-            with open('DictOfEnglW.txt', 'a+', encoding='utf-8') as f_obj:
-                f_obj.write(s + '\n')
-        else:
-            print('Вы ввели запись в неправильном формате.')
+words = []
+translations = []
+with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
+    for f in f_obj.readlines():
+        words.append(f.split(':')[0])
+        translations.append(f.strip().split(':')[1])
 
 
-def extraction():
-    key = []
-    val = []
-    with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
-        for f in f_obj.readlines():
-            key.append(f.split(':')[0])
-            val.append(f.strip().split(':')[1])
-    return key, val
+def add_value(value):
+    if value.find(':') >= 0:
+        with open('DictOfEnglW.txt', 'a+', encoding='utf-8') as f_obj:
+            f_obj.write(value + '\n')
+        return True
+    else:
+        return False
 
 
-def checker(word):
-    with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
-        for f in f_obj.readlines():
-            if f.split(':')[0] == word:
-                cor_tran = f.strip().split(':')[1]
-    return cor_tran
+def get_translate(user_word):
+    for idx, word in enumerate(words):
+        if user_word == word:
+            cor_tran = translations[idx]
+            return cor_tran
 
 
 def option_selection():
-    words, translations = extraction()
     word = random.choice(words)
-    translation = random.sample(translations, 3)
-    cor_tran = checker(word)
-    translation.append(cor_tran)
-    translation = set(translation)
+    translation = set()
+    cor_tran = get_translate(word)
+    translation.add(cor_tran)
     while len(translation) != 4:
-        with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
-            for f in f_obj.readlines():
-                translation.add(f.strip().split(':')[1])
+        translation.add(random.choice(translations))
     return word, translation
 
 
 def main():
     word, translation = option_selection()
-    cor_tran = checker(word)
+    cor_tran = get_translate(word)
     print(f'Как переводиться слово "{word}"?')
     for idx, el in enumerate(translation):
         print(f'{idx + 1}){el}')
@@ -66,18 +55,21 @@ def main():
     if answer == ans:
         print('Верный ответ.')
     else:
-        print(f'Ответ неверный. Верный ответ -{checker(word)}.')
+        print(f'Ответ неверный. Верный ответ -{cor_tran}.')
 
 
 def choice_time():
     tictac = input('Введите время в минутах:\n')
-    tictac = float(tictac)*60
+    tictac = float(tictac) * 60
     while True:
-        time.sleep(tictac)
         main()
+        time.sleep(tictac)
 
 
 if __name__ == '__main__':
-    add_value()
+    # add_value('sun:Солнце')
+    # add_value('word:Слово')
+    # add_value('kb:клавиатура')
+    # add_value('mouse:Мышь')
     #main()
     choice_time()
