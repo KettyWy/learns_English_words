096342e9c03b7b8ad3117cb9ce15f1987973eb2d
Kettywy <kettywy@mail.ru>
2022-03-26 01:46:21 +0300

    добавлен поиск, удаление

--- learns_English_words.py
+++ learns_English_words.py
@@ -1,22 +1,41 @@
 import time
 import random
 
-
 words = []
+weights = []
 translations = []
 with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
     for f in f_obj.readlines():
-        words.append(f.split(':')[0])
-        translations.append(f.strip().split(':')[1])
+        words.append(f.split(':')[:2])
+        weights.append(int(f.split(':')[0]))
+        translations.append(f.strip().split(':')[2])
 
 
 def add_value(value):
     if value.find(':') >= 0:
         with open('DictOfEnglW.txt', 'a+', encoding='utf-8') as f_obj:
-            f_obj.write(value + '\n')
+            f_obj.write('100:' + value + '\n')
         return True
     else:
         return False
+
+
+def del_value(value):
+    with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
+        data = f_obj.readlines()
+        data = filter(lambda line: value not in line, data)
+    with open('DictOfEnglW.txt', 'w', encoding='utf-8') as f_obj:
+        f_obj.write("".join(data))
+
+
+def find_word(word):
+    with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
+        data = f_obj.readlines()
+        for el in data:
+            if el.split(':').count(word) > 0:
+                return ' - '.join(el.strip().split(':')[1:])
+            # else:
+            #     return f'Нет такого слова.'
 
 
 def get_translate(user_word):
@@ -27,7 +46,7 @@ def get_translate(user_word):
 
 
 def option_selection():
-    word = random.choice(words)
+    word = random.choices(words, weights=weights)[0]
     translation = set()
     cor_tran = get_translate(word)
     translation.add(cor_tran)
@@ -38,8 +57,9 @@ def option_selection():
 
 def main():
     word, translation = option_selection()
+    old_word = f'{word[0]}:{word[1]}'
     cor_tran = get_translate(word)
-    print(f'Как переводиться слово "{word}"?')
+    print(f'Как переводиться слово "{word[1]}"?')
     for idx, el in enumerate(translation):
         print(f'{idx + 1}){el}')
         if el == cor_tran:
@@ -54,6 +74,15 @@ def main():
             print('Введите числа от 1 до 4.')
     if answer == ans:
         print('Верный ответ.')
+        if int(word[0]) > 0:
+            word[0] = int(word[0]) - 1
+        new_word = f'{word[0]}:{word[1]}'
+        with open('DictOfEnglW.txt', 'r', encoding='utf-8') as f_obj:
+            old_text = f_obj.read()
+            new_text = old_text.replace(old_word, new_word)
+        with open('DictOfEnglW.txt', 'w', encoding='utf-8') as f_obj:
+            f_obj.write(new_text)
+
     else:
         print(f'Ответ неверный. Верный ответ -{cor_tran}.')
 
@@ -67,9 +96,8 @@ def choice_time():
 
 
 if __name__ == '__main__':
-    # add_value('sun:Солнце')
-    # add_value('word:Слово')
-    # add_value('kb:клавиатура')
-    # add_value('mouse:Мышь')
-    #main()
-    choice_time()
+    # main()
+    # choice_time()
+    # add_value()
+    # del_value('kb')
+    print(find_word('word'))
